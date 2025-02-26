c5646eee5baebae0def4837076f9e430066e4193
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2025-02-27 01:48:37 +0530

    Update pgm.py

--- task 1/pgm.py
+++ task 1/pgm.py
@@ -1,10 +1,18 @@
+import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-text = """Natural Language Toolkit (NLTK) is one of the largest Python libraries for performing various Natural Language Processing tasks. 
-From rudimentary tasks such as text pre-processing to tasks likes vectorized representation of text – NLTK’s API has covered everything."""
+
+nltk.download('punkt')
+
+with open("sample.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
 
 sentences = sent_tokenize(text)
-words = word_tokenize(text)
+print("Sentence Tokenization:")
+for i, sentence in enumerate(sentences, 1):
+    print(f"{i}. {sentence}")
 
-print("Sentence Tokenization:", sentences)
-print("Word Tokenization:", words)
+words = word_tokenize(text)
+print("\nWord Tokenization:")
+print(words)
